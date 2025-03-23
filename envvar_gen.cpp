a5c22f4f1f90a08d5a2c6c9b538cd42c68477a1e
neko64 <neko64@outlook.jp>
2025-03-23 19:36:14 -0400

    env gen util

---
+++ envvar_gen.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <memory>
+#include <stdexcept>
+#include <sstream>
+#include <cstdlib>
+#include <array>
+#include <filesystem>
+using namespace std;
+
+int main() {
+
+    array<char, 2048> buf;
+    string home_dir;
+    stringstream ss;
+
+    
+    // Preamble
+    ss << "#!/bin/bash" << endl;
+
+    // retrieve HOME_DIR
+    unique_ptr<FILE, decltype(&pclose)> pipe(popen("realpath ~", "r"), pclose);
+    
+    if(!pipe) {
+        throw runtime_error("failed to run realpth command to get home dir");
+        exit(1);
+    }
+
+    while(fgets(buf.data(), buf.size(), pipe.get()) != nullptr) {
+        home_dir = buf.data();
+    }
+    if(!home_dir.empty() && home_dir.back() == '\n') {
+        home_dir.pop_back();
+    }
+
+    filesystem::path home_path = filesystem::path(home_dir);
+    filesystem::path tanuproj_path = home_path / "dev";
+    filesystem::path opt_path = filesystem::path("/opt");
+    filesystem::path libnekokan_path = opt_path / "libnekokan" / "lib";
+
+    // HOME_TMP_DIR
+    ss << "# HOME_TMP_DIR" << endl;
+    ss << "export HOME_TMP_DIR=" << (home_path / "tmp").string() << endl;
+
+    ss << "# TANUPROJ_HOME" << endl;
+    ss << "export TANUPROJ_HOME=" << tanuproj_path.string() << endl;
+
+    // NEKOKAN_INDB_PATH
+    ss << "# NEKOKAN_INDB_PATH" << endl;
+    ss << "export NEKOKAN_INDB_PATH=" << (home_path / "nekokan_indb").string() << endl;
+
+    // NEKOKAN_CODE_DIR
+    ss << "# NEKOKAN_CODE_DIR" << endl;
+    ss << "export NEKOKAN_CODE_DIR=" << (tanuproj_path / "nekokan").string() << endl; 
+
+    // NEKOKAN_BIN_DIR
+    ss << "# NEKOKAN_BIN_DIR" << endl;
+    ss << "export NEKOKAN_BIN_DIR=" << (home_path / "nekokan" / "bin").string() << endl;
+
+    // NEKOKAN_CONF_DIR
+    ss << "# NEKOKAN_CONF_DIR" << endl;
+    ss << "export NEKOKAN_CONF_DIR=" << (home_path / "nekokan" / "conf").string() << endl;
+
+    // LIBNEKOKAN_DIR
+    ss << "# LIBNEKOKAN_DIR" << endl;
+    ss << "export LIBNEKOKAN_DIR=" << libnekokan_path.string() << endl;
+
+    // TANULIB_CPP_CODE_DIR
+    ss << "# TANULIB_CPP_CODE_DIR" << endl;
+    ss << "export TANULIB_CPP_CODE_DIR=" << (tanuproj_path / "tanulib_cpp") << endl;
+
+    // NEKOKAN_ENV
+    ss << "# NEKOKAN_ENV" << endl;
+    ss << "export NEKOKAN_ENV=local" << endl;
+
+    // Update LD_LIBRARY_PATH
+    ss << "# Prepend LIBNEKOKAN_DIR to LD_LIBRARY_PATH" << endl;
+    ss << "export LD_LIBRARY_PATH=" << libnekokan_path.string() << ":${LD_LIBRARY_PATH}" << endl;
+
+    cout << ss.str() << endl;
+
+    // write the result to ~/.nekokan_include.bash
+    filesystem::path inc_file_path = home_path / ".nekokan_include.bash";
+    cout << "writing the above result to " << inc_file_path.string() << " ... " << endl;
+
+    if(filesystem::exists(inc_file_path)) {
+        if(!filesystem::remove(inc_file_path)) {
+            throw runtime_error("failed to remove " + inc_file_path.string());
+        }
+    }
+    ofstream fout;
+    fout.open(inc_file_path.string(), ios::out);
+    if(!fout.is_open()) {
+        throw runtime_error("failed to open " + inc_file_path.string());
+    }
+    fout << ss.str() << endl;
+    fout.close();
+    cout << "all done." << endl;
+}
---
+++ evgen.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+set -x
+
+
+if [ -e ./evgen ]; then
+    rm ./evgen
+fi
+g++-13 -std=c++20 envvar_gen.cpp -o evgen
